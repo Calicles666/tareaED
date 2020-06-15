7ce5671b09b27810cbbe73e4c105a7c3783f4c07
Abraham <calicles666@hotmail.com>
2020-06-15 12:53:52 +0200

    Abraham Garrido

--- JavaMutationTesting/src/main/java/io/github/picodotdev/blogbitix/mutationtesting/Passenger.java
+++ JavaMutationTesting/src/main/java/io/github/picodotdev/blogbitix/mutationtesting/Passenger.java
@@ -3,6 +3,8 @@ package io.github.picodotdev.blogbitix.mutationtesting;
 public class Passenger {
 
     private int age;
+    
+    System.out.println ("Hola");
 
     public Passenger(int age) {
         this.age = age;
